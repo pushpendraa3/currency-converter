af7e4b9d1a33fb6bea2b18aef410e6b127b32e2a
Pushpendra <pushpendrasingh.91650@gmail.com>
2024-02-25 12:57:27 +0530

    currency

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,83 @@
+import { useState } from 'react';
+import InputBox from './components/InputBox.jsx';
+import useCurrencyInfo from './hooks/useCurrencyInfo.js';
+
+const BackgroundImage = "";
+
 function App() {
+  const [from, setFrom] = useState("inr");
+  const [to, setTo] = useState("usd");
+  const [amount, setAmount] = useState(0);
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+  console.log(currencyInfo)
+  const options = Object.keys(currencyInfo);
+
+  const swapHandler = () => {
+    setFrom(to)
+    setTo(from)
+    setAmount(convertedAmount)
+    setConvertedAmount(amount)
+
+  }
+
+  const convertHandler = () => {
+    setConvertedAmount(amount * currencyInfo[to])
+  }
   return (
-    <>
-      <h1 className="text-3xl bg-orange-500 p-2 text-center m-4">Currency converter</h1>
-    </>
-  )
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url('${BackgroundImage}')`,
+      }}
+    >
+      <div className="w-full">
+        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              convertHandler()
+
+            }}
+          >
+            <div className="w-full mb-1">
+              <InputBox
+                label="From"
+                amount={amount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setAmount(amount)}
+                selectCurrency={from}
+              />
+            </div>
+            <div className="relative w-full h-0.5">
+              <button
+              onClick={swapHandler}
+                type="button"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+
+              >
+                swap
+              </button>
+            </div>
+            <div className="w-full mt-1 mb-4">
+            <InputBox
+                label="To"
+                amount={convertedAmount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setTo(currency)}
+                selectCurrency={from}
+                amountDisable
+              />
+            </div>
+            <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+              Convert {from} to {to}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App
--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -5,14 +5,15 @@ import { useEffect } from 'react'
 const useCurrencyInfo = (currency) => {
     const [data, setData] = useState({});
 
+    let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    
     useEffect(() => {
-        let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
 
-        fetch(url)
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
             .then((res) => res.json)
             .then((res) => setData(res[currency]))
     }, [currency])
-    console.log(data);
+    console.log(url);
     return data;
 }
 
