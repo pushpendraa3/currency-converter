618fc5269655c361e1368835133d4cb4ed09ca19
Pushpendra <pushpendrasingh.91650@gmail.com>
2024-02-26 13:45:02 +0530

    remove unused code in hook file

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,12 +1,8 @@
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 const useCurrencyInfo = (currency) => {
     const [data, setData] = useState({});
-
     useEffect(() => {
-
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
             .then((res) => res.json())
             .then((res) => setData(res[currency]))
